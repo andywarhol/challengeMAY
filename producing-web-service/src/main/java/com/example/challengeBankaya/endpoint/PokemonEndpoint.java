0785147726b284aa0165b562e4cd283c0467cee6
Andrea Urrea Urias <36893088+andywarhol@users.noreply.github.com>
2024-05-30 00:43:33 -0700

    added service to call api

--- producing-web-service/src/main/java/com/example/challengeBankaya/endpoint/PokemonEndpoint.java
+++ producing-web-service/src/main/java/com/example/challengeBankaya/endpoint/PokemonEndpoint.java
@@ -1,8 +1,11 @@
 package com.example.challengeBankaya.endpoint;
 
 import com.example.challengeBankaya.repo.PokemonRepository;
+import com.example.challengeBankaya.service.PokeApiService;
 import com.example.demosoap.GetPokemonRequest;
 import com.example.demosoap.GetPokemonResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -12,9 +15,14 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class PokemonEndpoint {
 
+    private static final Logger logger = LoggerFactory.getLogger(PokemonEndpoint.class);
+
     private static final String NAMESPACE_URI = "http://example.com/demosoap";
 
     private PokemonRepository pokemonRepository;
+
+    @Autowired
+    PokeApiService service;
 
     @Autowired
     public PokemonEndpoint (PokemonRepository pokemonRepository){
@@ -24,8 +32,9 @@ public class PokemonEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonRequest")
     @ResponsePayload
     public GetPokemonResponse getPokemonResponse(@RequestPayload GetPokemonRequest req){
-        GetPokemonResponse res = new GetPokemonResponse();
-        res.setPokemonEntity(pokemonRepository.findPokemon(req.getName()));
+        GetPokemonResponse res = service.getPokemonByName(req.getName());
+        logger.info(res.getPokemonEntity().getName());
+        //res.setPokemonEntity(pokemonRepository.findPokemon(req.getName()));
 
         return res;
     }
--- producing-web-service/src/main/java/com/example/challengeBankaya/service/PokeApiService.java
+++ producing-web-service/src/main/java/com/example/challengeBankaya/service/PokeApiService.java
@@ -1,17 +1,37 @@
 package com.example.challengeBankaya.service;
 
+import com.example.demosoap.GetPokemonResponse;
+import com.example.demosoap.PokemonEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
 public class PokeApiService {
 
-//    private final RestTemplate restTemplate = new RestTemplate();
-//
-//    public GetPokemonResponse getPokemonByName() {
-//       return getPokemonByName();
-//    }
-//
-//    public GetPokemonResponse getPokemonByName(String name){
-//        String apiUrl = UriComponentsBuilder.fromHttpUrl("https://pokeapi.co/api/v2/pokemon/" + name).toUriString();
-//        ResponseEntity<PokemonEntity> pokemonEntity = restTemplate.getForEntity(apiUrl, PokemonEntity.class);
-//
-//        return new GetPokemonResponse();
-//    }
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public GetPokemonResponse getPokemonByName() {
+       return getPokemonByName();
+    }
+
+    public GetPokemonResponse getPokemonByName(String name){
+
+        //Convert to lower case because pokeapi ignores uppercase names
+        String pokeName = name.toLowerCase();
+
+        String apiUrl = UriComponentsBuilder.fromHttpUrl("https://pokeapi.co/api/v2/pokemon/" + pokeName).toUriString();
+        ResponseEntity<PokemonEntity> pokemonResult = restTemplate.getForEntity(apiUrl, PokemonEntity.class);
+
+        if (pokemonResult.getStatusCode().toString().equals("200 OK")) {
+            PokemonEntity pokemonFound = pokemonResult.getBody();
+            GetPokemonResponse res = new GetPokemonResponse();
+            res.setPokemonEntity(pokemonFound);
+
+            return res;
+        }
+
+        return new GetPokemonResponse();
+    }
 }
